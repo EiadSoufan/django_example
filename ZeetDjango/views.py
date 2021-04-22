89bea1a3090ac190cb84adac68bca0f5ea23dc01
EiadSoufan <34572644+EiadSoufan@users.noreply.github.com>
2021-04-22 03:20:14 +0300

    Update views.py

--- ZeetDjango/views.py
+++ ZeetDjango/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-def home(request):
-    return render(request,"index.html")
+from django.http import HttpResponse,FileResponse
+
+
+def index(request):
+    return HttpResponse('index')
+	
+	
+def detail(request, category_id):
+    return HttpResponse('detail')
+	
+
+def pdffile(request, category_id,fid):
+    return HttpResponse('pdffile')
